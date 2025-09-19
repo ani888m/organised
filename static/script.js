9798a785b879e2803518d324f0a4f2931f470ccd
ani888m <antonet888@yahoo.de>
2025-09-19 13:18:01 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -166,9 +166,9 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-  window.addEventListener("scroll", function () {
+window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
-    if (window.scrollY > 80) {
+    if (window.scrollY > 50) {
       header.classList.add("shrink");
     } else {
       header.classList.remove("shrink");
