8501fe347ee01ac2873eb480bdd8953029b00684
ani888m <antonet888@yahoo.de>
2025-10-02 13:34:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ stripe.api_key = os.getenv("STRIPE_SECRET_KEY", "DEIN_STRIPE_SECRET_KEY")
 # ---------- SEITEN ----------
 @app.route('/')
 def index():
-    kategorienamen = ["Unsere Bücher", "Neuerscheinungen", "Über Angst", "Helga Bansch"]
+    kategorienamen = ["Unsere Bücher", "Neuerscheinungen", "Über Angst", "Helga Bansch", "Klassiker"]
 
     # Produkte aus JSON nach Kategorie gruppieren
     kategorien = [
