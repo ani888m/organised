67998a66b5e7092b83ade3d73426d354f2777852
ani888m <antonet888@yahoo.de>
2025-10-12 10:17:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -153,7 +153,11 @@ def danke():
     return render_template('danke.html')
 
 # ---------- START ----------
+
+
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Erstellt Tabellen automatisch, falls sie nicht existieren
-    app.run(debug=True)
+        db.create_all()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
