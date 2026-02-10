1ab8e72f34a74f9072cdac1d6fb112756e5d4f0c
ani888m <antonet888@yahoo.de>
2026-02-10 16:43:58 +0100

    Update orders.py

--- services/orders.py
+++ services/orders.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 import secrets
 from flask import current_app as app
-from .buchbutler import lade_rechnung
+from buchbutler import lade_rechnung
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 BESTELL_DB = os.path.join(basedir, "../bestellungen.db")
