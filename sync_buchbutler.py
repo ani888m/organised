e2281e32267e9797a64a446a31bb569906316fd1
ani888m <antonet888@yahoo.de>
2026-03-16 12:35:03 +0100

    Create sync_buchbutler.py

---
+++ sync_buchbutler.py
@@ -0,0 +1,22 @@
+
+# sync_buchbutler.py
+import os
+from app import app, db, lade_produkt_von_api, lade_bestand_von_api
+from models import Produkt
+
+with app.app_context():
+    alle_produkte = Produkt.query.all()
+    for produkt in alle_produkte:
+        api = lade_produkt_von_api(produkt.ean)
+        movement = lade_bestand_von_api(produkt.ean)
+
+        if api:
+            produkt.name = api.get("name")
+            produkt.autor = api.get("autor")
+        if movement:
+            produkt.preis = movement.get("preis")
+
+        db.session.add(produkt)
+
+    db.session.commit()
+    print("✅ Sync abgeschlossen")
