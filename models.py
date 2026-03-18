9922e47c8be57f9eaae4af539aa1ab2fe186681a
ani888m <antonet888@yahoo.de>
2026-03-18 16:18:20 +0100

    Update models.py

--- models.py
+++ models.py
@@ -6,22 +6,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
-
-    punkte = db.Column(db.Integer, default=0)
-
-    erstellt_am = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
 
 class Gutschein(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -35,15 +19,24 @@ class Gutschein(db.Model):
 # ----------------------
 # User Modell
 # ----------------------
+
+
+
+
 class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    punkte = db.Column(db.Integer, default=0)
+    erstellt_am = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return f"<User {self.email}>"
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 # ----------------------
 # Bestell-Modelle
