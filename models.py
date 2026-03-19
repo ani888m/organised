1b9856ec439640b5bf38a15e510a1c8921f65a46
ani888m <antonet888@yahoo.de>
2026-03-19 10:12:08 +0100

    Update models.py

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class Gutschein(db.Model):
     code = db.Column(db.String(50), unique=True)
     wert = db.Column(db.Float)
 
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id")) 
     eingelöst = db.Column(db.Boolean, default=False)
 
 # ----------------------
@@ -117,6 +117,9 @@ class Produkt(db.Model):
     lagerbestand = db.Column(db.Integer)
 
     bild_url = db.Column(db.String(500))
+        
+    slug = db.Column(db.String(255), unique=True, index=True)  # ✅ NEU
+
 
     zuletzt_aktualisiert = db.Column(
         db.DateTime,
